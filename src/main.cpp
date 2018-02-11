28fd9413d57fc447b283b76e64921ec7a7272886
Daniel <daniel.ks@outlook.de>
2018-02-11 10:23:30 +0100

    Außenline fade hinzugefügt

--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,13 @@
 #include "FastLED.h"
 #define DATA_PIN 6
 #define BRIGHTNESS  80
-#define UPDATES_PER_SECOND 60
 #define NUM_LEDS (kMatrixWidth * kMatrixHeight)
 
 const uint8_t kMatrixWidth = 15;
 const uint8_t kMatrixHeight = 15;
 
 int hue = 0;
-int Delay = 100;
+int Delay = 50;
 
 uint16_t XY( uint8_t x, uint8_t y)//reverse odd rows
 {
@@ -37,9 +36,10 @@ uint16_t XYsafe( uint8_t x, uint8_t y)
 }
 
 void setup() {
-  delay( 3000 ); // power-up safety delay
+  delay( 1000 ); // power-up safety delay
   FastLED.addLeds<WS2812B,DATA_PIN,GRB>(Led,NUM_LEDS);
   FastLED.setBrightness(  BRIGHTNESS );
+  Serial.begin(57600);
 }
 
 void loop()
@@ -121,8 +121,96 @@ void loop()
     FastLED.delay(Delay);
    }
 
-  Led[112] = CHSV(hue,255,255);
+  Led[112] = CHSV(hue,255,255); // Middel Dot
   FastLED.show();
-  hue++;
-  FastLED.delay(Delay);
+  FastLED.delay(200);
+
+  for (int j = 0; j < 135; ++j) //Middel Dot revese
+  {
+    Led[112].fadeToBlackBy(4);
+  }
+
+for (int i = 0; i < 2; ++i)//Ring 6 revese
+   {
+    for (int j = 0; j < 135; ++j)
+    {
+      Led[96+1-i] =
+      Led[128-1+i] = 
+      Led[XY(6,1+i+6)] =
+      Led[XY(8,1-i+6)].fadeToBlackBy(4);   
+    }
+    FastLED.delay(Delay);
+   }
+
+for (int i = 0; i < 4; ++i)//Ring 5 revese
+   {
+    for (int j = 0; j < 135; ++j)
+    {
+      Led[83-i] =
+      Led[141+i] = 
+      Led[XY(5,3-i+5)] =
+      Led[XY(9,1+i+5)].fadeToBlackBy(4);
+    }
+    FastLED.delay(Delay);
+   } 
+
+for (int i = 0; i < 6; ++i)//Ring 4 revese
+   {
+    for (int j = 0; j < 135; ++j)
+    {
+      Led[69-i] =
+      Led[155+i] = 
+      Led[XY(4,1+i+4)] =
+      Led[XY(10,5-i+4)].fadeToBlackBy(4);
+    }
+    FastLED.delay(Delay);
+  }
+
+for (int i = 0; i < 8; ++i)//Ring 3
+   {
+    for (int j = 0; j < 135; ++j)
+    {
+      Led[55-i] =
+      Led[169+i] = 
+      Led[XY(3,7-i+3)] =
+      Led[XY(11,1+i+3)].fadeToBlackBy(4);
+    }
+    FastLED.delay(Delay);
+  }
+
+for (int i = 0; i < 10; ++i)//Ring 2 revese
+   {
+    for (int j = 0; j < 135; ++j)
+    {
+      Led[41-i] =
+      Led[183+i] = 
+      Led[XY(2,1+i+2)] =
+      Led[XY(12,9-i+2)].fadeToBlackBy(4);
+    }
+    FastLED.delay(Delay);
+  }
+
+for (int i = 0; i < 12; ++i)//Ring 1 revese
+   {
+    for (int j = 0; j < 135; ++j)
+    {
+      Led[27-i] =
+      Led[197+i] = 
+      Led[XY(1,11-i+1)] =
+      Led[XY(13,1+i+1)].fadeToBlackBy(4);
+    }
+    FastLED.delay(Delay);
+  }
+
+for (int i = 0; i < 14; ++i)//Ring 0 revese
+   {
+    for (int j = 0; j < 135; ++j)
+    {
+      Led[13-i] =
+      Led[211+i] = 
+      Led[XY(0,1+i)] =
+      Led[XY(14,13-i)].fadeToBlackBy(4);
+    }
+    FastLED.delay(Delay);
+  }
 }
